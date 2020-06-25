a39a4148fb3e99ffeee75aae197d0051daed24ab
LornelleKAMTA <merveillelornellekamta@gmail.com>
2020-06-25 12:51:07 +0200

    Implementation des methodes

--- smart-city-merveille/src/main/java/com/episen/smartcitymerveille/AppController.java
+++ smart-city-merveille/src/main/java/com/episen/smartcitymerveille/AppController.java
@@ -4,20 +4,41 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
-	
+
 	@Autowired
 	private CapteurService service;
-	
+
 	@RequestMapping("/")
 	public String viewHomePage(Model model) {
-	    List<Capteur> listCapteurs = service.listAll();
-	    model.addAttribute("listCapteurs", listCapteurs);
+		List<Capteur> listCapteurs = service.listAll();
+		model.addAttribute("listCapteurs", listCapteurs);
+
+		return "index";
+	}
+	
+	@RequestMapping("/new")
+	public String showNewCapteurPage(Model model) {
+	    Capteur capteur = new Capteur();
+	    model.addAttribute("capteur", capteur);
 	     
-	    return "index";
+	    return "new_capteur";
 	}
 
-}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveCapteur_Polluant(@ModelAttribute("capteur") Capteur capteur) {
+	    service.save(capteur);
+	     
+	    return "redirect:/";
+	}
+
+	}
+
+
