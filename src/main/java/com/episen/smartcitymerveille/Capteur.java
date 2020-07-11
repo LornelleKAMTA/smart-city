300ab6f51f2da5c55ad6cb44cb8c2a64917dea28
LornelleKAMTA <merveillelornellekamta@gmail.com>
2020-07-11 11:13:22 +0200

    Methode de Configuration

--- src/main/java/com/episen/smartcitymerveille/Capteur.java
+++ src/main/java/com/episen/smartcitymerveille/Capteur.java
@@ -10,8 +10,13 @@ public class Capteur {
 	private Long id;
 	private String adresse_ip;
 	private String localisation;
-	
-	protected Capteur() {
+	private String seuil_co2;
+	private String seuil_no2;
+	private String seuil_pf;
+	private String seuil_min_tmp;
+	private String seuil_max_tmp;
+
+protected Capteur() {
 		
 	}
 	
@@ -40,6 +45,44 @@ public class Capteur {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+	public String getSeuil_co2() {
+		return seuil_co2;
+	}
+
+	public void setSeuil_co2(String seuil_co2) {
+		this.seuil_co2 = seuil_co2;
+	}
+
+	public String getSeuil_no2() {
+		return seuil_no2;
+	}
+
+	public void setSeuil_no2(String seuil_no2) {
+		this.seuil_no2 = seuil_no2;
+	}
+
+	public String getSeuil_pf() {
+		return seuil_pf;
+	}
+
+	public void setSeuil_pf(String seuil_pf) {
+		this.seuil_pf = seuil_pf;
+	}
+
+	public String getSeuil_min_tmp() {
+		return seuil_min_tmp;
+	}
+
+	public void setSeuil_min_tmp(String seuil_min_tmp) {
+		this.seuil_min_tmp = seuil_min_tmp;
+	}
+
+	public String getSeuil_max_tmp() {
+		return seuil_max_tmp;
+	}
+
+	public void setSeuil_max_tmp(String seuil_max_tmp) {
+		this.seuil_max_tmp = seuil_max_tmp;
+	}
 	
 }
